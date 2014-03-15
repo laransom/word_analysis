1b6543cdc9b8191f50b2d2fdf77a7a5f8c9154cc
laransom <laransom@gmail.com>
2014-03-15 18:47:12 -0400

    Made WordAnalysis class and have an operational rspec file on it

--- lib/word_checker.rb
+++ lib/word_checker.rb
@@ -1,5 +1,92 @@
 require 'pry'
 
 class WordAnalysis
+  attr_reader :string
+
+  def initialize(string)
+    @string = string
+  end
+
+  def word_count
+    word_count_hash = {}
+    @string.gsub('\n', ' ').scan(/\w[\w\'\-]*/).each do |word|
+      if word =~ /[A-z]/
+        if word_count_hash.key?(word.downcase) == false
+          word_count_hash[word.downcase] = 1
+        else
+          word_count_hash[word.downcase] += 1
+        end
+      end
+    end
+    word_count_hash
+  end
+
+  def letter_count
+    letter_count_hash = {}
+    @string.gsub(' ', '').gsub('\n', '').split('').each do |letter|
+      if letter =~ /[A-z]/
+        if letter_count_hash.key?(letter.downcase) == false
+          letter_count_hash[letter.downcase] = 1
+        else
+          letter_count_hash[letter.downcase] += 1
+        end
+      end
+    end
+    letter_count_hash
+  end
+
+  def symbol_count
+    symbol_count_hash = {}
+    @string.gsub(' ', '').gsub('\n', '').split('').each do |symbol|
+      if symbol =~ /\W/
+        if symbol_count_hash.key?(symbol) == false
+          symbol_count_hash[symbol] = 1
+        else
+          symbol_count_hash[symbol] += 1
+        end
+      end
+    end
+    symbol_count_hash
+  end
+
+  def common_words
+    sorted_words = word_count.sort_by { |word, amt| amt}
+    [sorted_words[-1][0], sorted_words[-2][0], sorted_words[-3][0]]
+  end
+
+  def common_letters
+    sorted_letters = letter_count.sort_by { |letter, amt| amt}
+    [sorted_letters[-1][0], sorted_letters[-2][0], sorted_letters[-3][0]]
+  end
+
+  def single_use_words
+    single_hash = word_count
+    single_hash.each do |letter, amt|
+      if amt > 1
+        single_hash.delete(letter)
+      end
+    end
+    single_hash.keys
+  end
+
+  def omitted_letters
+    all_letters = [*('a'..'z')]
+    used_letters_hash = letter_count
+    omitted_letters = []
+    all_letters.each do |letter|
+      if used_letters_hash.key?(letter.downcase) == false
+        omitted_letters << letter
+      end
+    end
+    omitted_letters
+  end
 
 end
+
+
+
+
+
+
+
+
--- spec/word_checker_spec.rb
+++ spec/word_checker_spec.rb
@@ -1,29 +1,102 @@
 require 'rspec'
 require_relative '../lib/word_checker.rb'
 
-describe WordAnalysis
-  attr_reader :string
+describe WordAnalysis do
+   let(:paragraph) {WordAnalysis.new("Simple simple. Are those the same, the same.
+    Symbols and numbers & $54 2 99.99%. Can't, won't stop.")}
 
-  def initialize(string)
-    @string = string
+  it 'Is a proper string that is input' do
+    expect(paragraph.string.class).to eq(String)
   end
 
-  def word_count
-    word_count_hash = {}
-    @string.gsub('\n', ' ')split.each do |word|
-      if word_count_hash.key?(word) == false
-        word_count_hash[word] = 1
-      else
-        word_count_hash[word] += 1
-      end
+  describe 'word_count' do
+
+    it 'Counts the words properly even if they have capitals or punctuation' do
+      expect(paragraph.word_count['simple']).to eq(2)
     end
+
+    it 'Does not count numbers as words' do
+      expect(paragraph.word_count['54']).to eq(nil)
+    end
+
+    it 'Does not count symbols as words' do
+      expect(paragraph.word_count['&']).to eq(nil)
+    end
+
   end
 
-  def letter_count
-    letter_count_hash = {}
-    @string.gsub('\n' | ' ', '').split('').each do |letter|
+  describe 'letter_count' do
+
+    it 'Counts letters properly' do
+      expect(paragraph.letter_count['s']).to eq(9)
+    end
+
+    it 'Does not count non letters' do
+      expect(paragraph.letter_count['9']).to eq(nil)
+    end
 
   end
+
+  describe 'symbol_count' do
+
+    it 'Counts symbols properly' do
+      expect(paragraph.symbol_count['$']).to eq(1)
+    end
+
+    it 'Does not count letters as symbols' do
+      expect(paragraph.symbol_count['a']).to eq(nil)
+    end
+
+  end
+
+  describe 'common_words' do
+
+    it 'returns an array of the top three most commons words' do
+      expect(paragraph.common_words).to eq(['simple','the','same'])
+    end
+
+  describe 'common_letters' do
+
+    it 'returns an array of the top three most common letters' do
+      expect(paragraph.common_letters).to eq(['s', 'e', 'm'])
+    end
+
+  end
+
+  describe 'single_use_words' do
+    let(:paragraph_two) {WordAnalysis.new("Some some words words. Are only used once")}
+
+    it 'returns an array of all words only used once' do
+      expect(paragraph_two.single_use_words).to eq(['are', 'only', 'used', 'once'])
+    end
+
+  end
+
+  describe 'omitted_letters' do
+    let(:paragraph_three) {WordAnalysis.new("Abc, D'ef. Hijklm nop qrstuv. WX!! Yz.")}
+
+    it 'returns an array of all omitted words' do
+      expect(paragraph_three.omitted_letters).to eq(['g'])
+    end
+
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 end
